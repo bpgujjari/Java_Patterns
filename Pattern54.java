0a1fa84abbaeaac17b42fa0adab9da9a6dce774e
bp <gujjaribhavaniprasad40@gmail.com>
2024-08-16 22:24:00 +0530

    new patterns updated

---
+++ Pattern54.java
@@ -0,0 +1,23 @@
+package com.comeback.patterns;
+
+import java.util.Iterator;
+
+public class Pattern54{
+	public static void main(String[] args) {
+		for (int i =5; i>=1; i--) {
+			
+			for (int j=i;j<5; j++) {
+				System.out.print(" ");
+			}
+			
+			for (int j =i; j>=1; j--) {
+				System.out.print(j);
+			}
+			
+			
+			System.out.println();
+			
+		}
+	}
+
+}
